8b7acbbdbd3f70316387961caa49e023228402bd
Chai-YD <1030333788@qq.com>
2018-08-19 06:12:56 -0400

    全排列

---
+++ my_prev_permutation_test.cpp
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////
+//prev_permutation
+//////////////////////////////////////////////////////
+
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main(){
+//    int arr[] = {1,2,3};
+//    reverse(arr,arr+3);
+//    do{
+//        cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
+//    }while(prev_permutation(arr,arr+3));
+//    return 0;
+//}
+//
+
+/////////////////////////////////////////////
+//next_permutation
+/////////////////////////////////////////////
+
+////true如果函数可以重新排列对象作为词典更大的排列
+////负责，函数返回false以指示排列不大于前一个，但最低可能（按升序排序）
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main(){
+//    int arr[] = {1,2,3};
+//    do{
+//        cout<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<endl;
+//    }while(next_permutation(arr,arr+3));
+//    return 0;
+//}
+
