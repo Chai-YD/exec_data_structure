5d22f2233277079fc54b906017fe4e4f021e20a2
Chai-YD <1030333788@qq.com>
2018-08-27 03:08:34 -0400

    字符串的逆置

---
+++ use_8_27.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<algorithm>
+//#include<string>
+//using namespace std;
+//
+//int main(){
+//    string str;
+//    getline(cin,str);
+//    int s = str.size();
+//    reverse(str.begin(),str.end());
+//    int i = 0;
+//    int j = 0;
+//    for(;i < s;++i){
+//        if(*(str.begin() + i) == ' '){
+//            reverse(str.begin()+j,str.begin()+i);
+//            j = i + 1;
+//        }
+//    }
+//    reverse(str.begin()+j,str.end());
+//    cout<<str<<endl;
+//}
