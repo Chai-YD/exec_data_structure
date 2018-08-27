93d3555c38aa9e3ef8fbf0365bccc15ec7e5e83e
Chai-YD <1030333788@qq.com>
2018-08-27 03:24:57 -0400

    从一个字符串中删除在另一个字符串中出现的字符

--- use_8_27.cpp
+++ use_8_27.cpp
@@ -19,3 +19,29 @@
 //    reverse(str.begin()+j,str.end());
 //    cout<<str<<endl;
 //}
+//
+//从一个字符串中去除另一个字符串中出现的字符
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//    string str;
+//    string tmp;
+//    getline(cin,str);
+//    getline(cin,tmp);
+//    cout<<str<<endl;
+//    cout<<tmp<<endl;
+//    int s = str.size();
+//    int i = 0;
+//    while(i < str.size()){
+//        if(tmp.find(str[i]) != -1){
+//            str.erase(i,1);
+//            continue;
+//        }
+//        ++i;
+//    }
+//    cout<<str<<endl;
+//    cout<<tmp<<endl;
+//    return 0;
+//}
+
