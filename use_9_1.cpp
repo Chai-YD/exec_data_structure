0d0d34bb54881b0f5b055c5467df79e9a57c771e
Chai-YD <1030333788@qq.com>
2018-09-01 08:09:32 -0400

    sort\unique\for_each算法的使用

--- use_9_1.cpp
+++ use_9_1.cpp
@@ -1,11 +1,38 @@
-//STL中算法replace 的使用
+////STL中算法replace 的使用
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int main(){
+//    string str = "aaa nihao";
+//    replace(str.begin(),str.end(),'a','c');
+//    cout<<str<<endl;
+//    return 0;
+//}
+
+//去除重复的元素
 #include<iostream>
+#include<vector>
 #include<algorithm>
 using namespace std;
 
+void output(int val){
+    cout<<val<<endl;
+}
+
 int main(){
-    string str = "aaa nihao";
-    replace(str.begin(),str.end(),'a','c');
-    cout<<str<<endl;
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    vec.push_back(2);
+    sort(vec.begin(),vec.end());
+    unique(vec.begin(),vec.end());
+    for_each(vec.begin(),vec.end(),output);
     return 0;
 }
+
+
+
+
