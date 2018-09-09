024ee7d76b0eedd01c62af4aee61efc9c9a07a28
Chai-YD <1030333788@qq.com>
2018-09-08 21:38:33 -0400

    strtok的使用

--- use_9_4.c
+++ use_9_4.c
@@ -1,19 +1,34 @@
-//strtok函数的使用
-#include <string.h>
-#include <stdio.h>
-
-char string[] = "A string\tof ,,tokens\nand some  more tokens";
-char seps[]   = " ,\t\n";
-char *token;
-
-int main( void )
-{
-   printf( "%s\n\nTokens:\n", string );
-   token = strtok( string, seps );
-   while( token != NULL ){
-       printf(" %s\n",token);
-       token = strtok(NULL,seps);
-   }
-   return 0;
-}
+////strtok函数的使用
+//#include <string.h>
+//#include <stdio.h>
+//
+//char string[] = "A string\tof ,,tokens\nand some  more tokens";
+//char seps[]   = " ,\t\n";
+//char *token;
+//
+//int main( void )
+//{
+//   printf( "%s\n\nTokens:\n", string );
+//   token = strtok( string, seps );
+//   while( token != NULL ){
+//       printf(" %s\n",token);
+//       token = strtok(NULL,seps);
+//   }
+//   return 0;
+//}
    
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    char string[] = "I am Student.";
+    char seqs[] = " ";
+    char* token;
+    token = strtok(string,seqs);
+    while(token != NULL){
+        printf("%s \n",token);
+        token = strtok(NULL,seqs);
+    }
+    return 0;
+}
