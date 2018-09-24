1749b71d9d85747ff75795551c873867c9b81cab
Chai-YD <1030333788@qq.com>
2018-09-24 02:51:49 -0400

    algorithm的使用

---
+++ my_algorithm_test.cpp
@@ -0,0 +1,26 @@
+////////////////////////////////
+//find的使用
+////////////////////////////////
+
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main(){
+//    int arr[] = {10,20,30,40};
+//    int *p;
+//    p = find(arr,arr+4,30);
+//    if(p != arr+4){
+//        cout<<*p<<endl;
+//    }else{
+//        cout<<"Not find"<<endl;
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+
+
